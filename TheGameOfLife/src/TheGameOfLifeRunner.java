c488de4adce6e5e07dc895ca8733a492c134a8f8
seth.novosel <seth.novosel@MU-C3-19.mullen.pvt.k12.co.us>
2015-02-06 12:30:17 -0700

    New changes to runner.

--- TheGameOfLife/src/TheGameOfLifeRunner.java
+++ TheGameOfLife/src/TheGameOfLifeRunner.java
@@ -7,21 +7,22 @@ public static void main(String[] args)
 {
 int spin = Spinner.playerSpin();
 Spaces.fillSpaces();
-//System.out.println("You are at " + Spaces.spaces.get(location).getName());
 move(spin);
 }
 public static void move(int s)
 {
-Scanner userInput = new Scanner(System.in);
+for(int i = 0; i < Spaces.spaces.size(); i++)
+{
 System.out.println("You landed on " + Spaces.spaces.get(location).getName());
 location += s;
-System.out.println("You landed on " + Spaces.spaces.get(location).getName());
+}
+Scanner userInput = new Scanner(System.in);
 String answer = userInput.nextLine();
 if(answer == "")
 	{
 	Spinner.playerSpin();
 	System.out.println("You landed on " + Spaces.spaces.get(location).getName());
-	//location = Spinner.spin;
+	//location = s;
 	location++;
 	}
 }
