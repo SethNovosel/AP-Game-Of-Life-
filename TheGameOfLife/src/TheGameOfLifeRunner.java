584af8608cbcaa882681feda9f0c9b0733269dab
Novosel Family <novoselse@mullenmail.com>
2015-02-10 22:37:21 -0700

    Order of if statements

--- TheGameOfLife/src/TheGameOfLifeRunner.java
+++ TheGameOfLife/src/TheGameOfLifeRunner.java
@@ -18,47 +18,7 @@ public class TheGameOfLifeRunner
 			{
 			System.out.println();
 			location += s;
-			if(location + s > 18)
-				{
-				location = 18;
-				System.out.println("You are now on " + Spaces.allSpaces[location]);
-				if(Spaces.allSpaces[location] == "Get House")
-					{
-					BuyHouse.addHouses();
-					new BuyHouse(null, i).playerHouse();
-					p.setWallet(p.getWallet() - BuyHouse.houses.get(BuyHouse.answer - 1).getPrice());
-					System.out.println("Your earnings are: " + p.getWallet());
-					onward();
-					System.out.println();
-					}
-				}
-			else if(location + s > 8)
-				{
-				location = 8;
-				System.out.println("You are now on " + Spaces.allSpaces[location]);
-				if(Spaces.allSpaces[location] == "Get Married")
-					{
-					new GetMarried().getSpouse();
-					onward();
-					System.out.println();
-					}
-				}
-		else if(location + s > 2)
-			{
-			location = 2;
-			System.out.println("You are now on " + Spaces.allSpaces[location]);
-			if(Spaces.allSpaces[location] == "Get Career")
-				{
-				PlayerCareer.addCareers();
-				Payday.addSalaries();
-				new PlayerCareer(null).playerCareer();
-				p.setSalary(Payday.salaries.get(Payday.randomSalary).getSalary());
-				System.out.println("Your salary is: " + p.getWallet());
-				onward();
-				System.out.println();
-				}
-			}
-		else if(Spaces.allSpaces[location] == "Normal Space")
+			if(Spaces.allSpaces[location] == "Normal Space")
 				{
 				onward();
 				System.out.println();
@@ -81,7 +41,7 @@ public class TheGameOfLifeRunner
 				onward();
 				System.out.println();
 				}
-			else if(Spaces.allSpaces[location] == "Have Kid")
+			if(Spaces.allSpaces[location] == "Have Kid")
 				{
 				new Kids().haveKids();
 				p.setKids(p.getKids() + Kids.answer);
@@ -95,6 +55,97 @@ public class TheGameOfLifeRunner
 				System.out.println("Your total earnings are: " + p.getWallet());
 				System.out.println();
 				break;
+				}
+			else if(location + s > 18)
+				{
+				location = 18;
+				System.out.println("You are now on " + Spaces.allSpaces[location]);
+				if(Spaces.allSpaces[location] == "Get House")
+					{
+					BuyHouse.addHouses();
+					new BuyHouse(null, i).playerHouse();
+					p.setWallet(p.getWallet() - BuyHouse.houses.get(BuyHouse.answer - 1).getPrice());
+					System.out.println("Your earnings are: " + p.getWallet());
+					onward();
+					System.out.println();
+					}
+				}
+			else if(Spaces.allSpaces[location] == "Normal Space")
+				{
+				onward();
+				System.out.println();
+				}
+			else if(Spaces.allSpaces[location] == "Payday")
+				{
+				Payday.addSalaries();
+				new Payday(i).payday();
+				p.setWallet(p.getWallet() + p.getSalary());
+				System.out.println("Your earnings are: " + p.getWallet());
+				onward();
+				System.out.println();
+				}
+			else if(Spaces.allSpaces[location] == "Life Space")
+				{
+				LifeSpots.addLifeCards();
+				new LifeSpots(null, i).lifeCard();
+				p.setWallet(p.getWallet() + LifeSpots.lifeCards.get(LifeSpots.randomCard).getReward());
+				System.out.println("Your earnings are: " + p.getWallet());
+				onward();
+				System.out.println();
+				}
+			else if(location + s > 8)
+				{
+				location = 8;
+				System.out.println("You are now on " + Spaces.allSpaces[location]);
+				if(Spaces.allSpaces[location] == "Get Married")
+					{
+					new GetMarried().getSpouse();
+					onward();
+					System.out.println();
+					}
+				}
+			else if(Spaces.allSpaces[location] == "Normal Space")
+				{
+				onward();
+				System.out.println();
+				}
+			else if(Spaces.allSpaces[location] == "Payday")
+				{
+				Payday.addSalaries();
+				new Payday(i).payday();
+				p.setWallet(p.getWallet() + p.getSalary());
+				System.out.println("Your earnings are: " + p.getWallet());
+				onward();
+				System.out.println();
+				}
+			else if(Spaces.allSpaces[location] == "Life Space")
+				{
+				LifeSpots.addLifeCards();
+				new LifeSpots(null, i).lifeCard();
+				p.setWallet(p.getWallet() + LifeSpots.lifeCards.get(LifeSpots.randomCard).getReward());
+				System.out.println("Your earnings are: " + p.getWallet());
+				onward();
+				System.out.println();
+				}
+			else if(location + s > 2)
+				{
+				location = 2;
+				System.out.println("You are now on " + Spaces.allSpaces[location]);
+				if(Spaces.allSpaces[location] == "Get Career" && location + s > 2)
+					{
+					PlayerCareer.addCareers();
+					Payday.addSalaries();
+					new PlayerCareer(null).playerCareer();
+					p.setSalary(Payday.salaries.get(Payday.randomSalary).getSalary());
+					System.out.println("Your salary is: " + p.getWallet());
+					onward();
+					System.out.println();
+					}
+				}
+			else if(Spaces.allSpaces[location] == "Normal Space")
+				{
+				onward();
+				System.out.println();
 				}
 			}
 		}
